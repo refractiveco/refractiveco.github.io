5c03516b6a324b4e4df20bc91f6996f27dc02e77
Rob Dunne <rob@refractive.co>
2016-02-18 12:13:22 +0000

    Homepage layout update

---
+++ js/refractive.js
@@ -0,0 +1,14 @@
+$(function() {
+	$('.home-header h1').animate({
+		opacity: 1
+	},4000);
+
+	setTimeout(function() {
+		$(".big-button").animate({
+			opacity: 1,
+			top: "-=30"
+		}, 1000, function() {
+			// Animation complete.
+		});
+	}, 1000);
+});
